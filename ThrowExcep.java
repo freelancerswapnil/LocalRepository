d45b89032cee9ae4b01d3f4ec964021b61785a4d
freelancerswapnil <156289815+freelancerswapnil@users.noreply.github.com>
2024-04-24 06:59:43 +0530

    try and catch statement example

---
+++ ThrowExcep.java
@@ -0,0 +1,26 @@
+class ThrowExcep {
+	static void help()
+	{
+		try {
+			throw new NullPointerException("error_unknown");
+		}
+		catch (NullPointerException e) {
+			System.out.println("Caught inside help().");
+			// rethrowing the exception
+			throw e;
+		}
+	}
+
+	public static void main(String args[])
+	{
+		try {
+			help();
+		}
+		catch (NullPointerException e) {
+			System.out.println(
+				"Caught in main error name given below:");
+			System.out.println(e);
+		}
+	}
+}
+
