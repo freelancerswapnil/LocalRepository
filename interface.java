d6c46c8f194471473146a167242128470bee3757
freelancerswapnil <156289815+freelancerswapnil@users.noreply.github.com>
2024-05-12 14:21:27 +0530

    interface class

--- interface.java
+++ interface.java
@@ -1,20 +1,14 @@
 import java.io.*;
 
-// A simple interface
 interface In1 {
   
-    // public, static and final
-    final int a = 10;
+   final int a = 10;
 
-    // public and abstract
     void display();
 }
 
-// A class that implements the interface.
 class TestClass implements In1 {
   
-    // Implementing the capabilities of
-    // interface.
     public void display(){ 
       System.out.println("Geek"); 
     }
