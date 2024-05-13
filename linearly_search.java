6f1a68b5cf5fd98042b23daf804cd5ef4b0bbdc5
freelancerswapnil <156289815+freelancerswapnil@users.noreply.github.com>
2024-05-13 07:32:50 +0530

    Java code for linearly search x in arr[]

--- linearly_search.java
+++ linearly_search.java
@@ -1,6 +1,4 @@
-// Java code for linearly search x in arr[]. If x 
-// is present then return its location, otherwise 
-// return -1 
+
 class LinearSearch { 
 	// This function returns index of element x in arr[] 
 	static int search(int arr[], int n, int x) 
